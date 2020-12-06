03691bdd5fb4f6d80de3e4465331ddb5f712dcbe
dilanyilmaz <51158449+dilanyilmaz@users.noreply.github.com>
2020-12-06 19:24:36 +0300

    first commit

---
+++ layouts/main.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home-page">
+             <header1/>
+             <megaMenu/>
+        <nuxt/>
+           
+    </div>
+</template>
+
+<script>
+
+import header1 from "~/components/header"
+import megaMenu from "~/components/megaMenu"
+
+export default {
+    components:{
+        header1,
+        megaMenu
+    }
+}
+</script>
+<style>
+ul {
+list-style: none;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Anasayfa/>
+    </div>
+</template>
+<script>
+import Anasayfa from "../pages/Anasayfa"
+
+export default {
+    components:{
+        Anasayfa
+    }
+}
+</script>
